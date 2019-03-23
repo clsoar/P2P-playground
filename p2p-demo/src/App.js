a9c9a9e542b5c0add9dbca1d7c0060858739fe08
CherieLee-Mason <clsoar@gmail.com>
2019-03-23 17:47:18 -0500

    Peerjs connection initialize

--- p2p-demo/src/App.js
+++ p2p-demo/src/App.js
@@ -1,11 +1,90 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Peer from "peerjs";
 import Host from './Host.js';
 import Player from './Player.js';
 import Landing from './Landing.js';
 
+/*
+var lastPeerId = null;
+var peer = null; // Own peer object
+var peerId = null;
+var conn = null;
+var recvId = document.getElementById("receiver-id");
+var status = document.getElementById("status");
+var players = [];
+*/
+
+
+
+/**
+ * Create the Peer object for our end of the connection.
+ *
+ * Sets up callbacks that handle any events related to our
+ * peer object.
+ */
+ /*function initialize() {
+    // Create own peer object with connection to shared PeerJS server
+    peer = new Peer(null, {
+        debug: 2
+    });
+
+    peer.on('open', function (id) {
+        // Workaround for peer.reconnect deleting previous id
+        if (peer.id === null) {
+            console.log('Received null id from peer open');
+            peer.id = lastPeerId;
+        } else {
+            lastPeerId = peer.id;
+        }
+
+        console.log('ID: ' + peer.id);
+        recvId.innerHTML = "ID: " + peer.id;
+        status.innerHTML = "Awaiting connection...";
+    });
+    peer.on('connection', function (c) {
+        // Allow only a single connection
+      /*  if (conn) {
+            c.on('open', function() {
+                c.send("Already connected to another client");
+                setTimeout(function() { c.close(); }, 500);
+            });
+            return;
+        }*/
+/*
+        conn = c;
+        players.push(conn);
+        console.log("Connected to: " + conn.peer);
+        status.innerHTML = "Connected"
+        //ready();
+        console.log(players);
+    });
+    peer.on('disconnected', function () {
+        status.innerHTML = "Connection lost. Please reconnect";
+        console.log('Connection lost. Please reconnect');
+
+        // Workaround for peer.reconnect deleting previous id
+        peer.id = lastPeerId;
+        peer._lastServerId = lastPeerId;
+        peer.reconnect();
+    });
+    peer.on('close', function() {
+        conn = null;
+        status.innerHTML = "Connection destroyed. Please refresh";
+        console.log('Connection destroyed');
+    });
+    peer.on('error', function (err) {
+        console.log(err);
+        alert('' + err);
+    });
+};
+*/
+
 class App extends Component {
+
+
+
   render() {
     return (
       <Router>
--- p2p-demo/src/Host.js
+++ p2p-demo/src/Host.js
@@ -1,7 +1,90 @@
 import React, { Component } from 'react';
 import './App.css';
+import Peer from 'peerjs';
+
+//var lastPeerId = null;
+
 
 class Host extends Component {
+
+  static defaultProps = {
+    peer: new Peer(null, {
+        debug: 2
+    })
+  }
+
+  state = {
+    peer: new Peer(null, {
+        debug: 2
+    }),
+    conn: null,
+    players: []
+  }
+
+  initialize = () => {
+    // Create own peer object with connection to shared PeerJS server
+  //  this.setState({
+
+  //  })
+
+
+    this.state.peer.on('open', (id) => {
+      // Workaround for peer.reconnect deleting previous id
+      // modified for react, vanilla JS credit to: https://github.com/jmcker/Peer-to-Peer-Cue-System
+      /*if (this.state.peer.id === null) {
+          console.log('Received null id from peer open');
+          this.setState({peer.id: lastPeerId});
+      } else {
+          lastPeerId = this.props.peer.id;
+      }*/
+
+      //handle connection message
+      console.log("ID: " + this.props.peer.id);
+
+
+    });
+
+    this.props.peer.on('connection', (c) => {
+      this.setState({conn: c});
+      this.setState({
+        players: this.state.players.push(this.state.conn)
+      });
+      console.log("Connected to: " + this.state.conn.peer);
+      //ready()
+      console.log(this.state.players);
+    });
+
+    this.props.peer.on('disconnected', () => {
+      //handle connection message
+      console.log("Connection lost. Please reconnect");
+
+
+    // Workaround for peer.reconnect deleting previous id
+    // modified for react, vanilla JS credit to: https://github.com/jmcker/Peer-to-Peer-Cue-System
+    /*this.setState({
+      peer.id: lastPeerId;
+    })
+    peer._lastServerId = lastPeerId;*/
+      this.state.peer.reconnect();
+    });
+
+    this.state.peer.on('close', () => {
+      this.setState({conn: null});
+      //handle connection message
+      console.log('Connection destroyed');
+    });
+
+    this.state.peer.on('error', (err) => {
+      console.log(err);
+      alert(''+ err);
+    })
+  }
+
+  componentDidMount() {
+
+    this.initialize();
+  }
+
   render() {
     return (
       <div className="Host">
@@ -12,9 +95,9 @@ class Host extends Component {
           <section className="connection-info">
             <div className="id-instructions">Use this ID for players to connect to host.</div>
             <div id="receiver-id-label" className="receiver-id-label">ID: </div>
-            <div id="receiver-id" className="receiver-id">None</div>
+            <div id="receiver-id" className="receiver-id">{this.props.peer.id}</div>
             <ol className="connected=players">Connected Players:
-              <li className="no-players player-list-item" id="no-players">Waiting for players to connect</li>
+              <li className="no-players player-list-item" id="no-players">{(this.state.players.length > 0) ? this.state.players : "Waiting for players to connect"}</li>
             </ol>
           </section>
           <section className="data-section">
