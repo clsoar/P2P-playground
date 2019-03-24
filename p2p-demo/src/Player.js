87c4f4c0c81e4a314fa554f75dbdb2b6c4879f9c
CherieLee-Mason <clsoar@gmail.com>
2019-03-23 19:01:03 -0500

    Player initialize and join

--- p2p-demo/src/Player.js
+++ p2p-demo/src/Player.js
@@ -1,7 +1,146 @@
 import React, { Component } from 'react';
 import './App.css';
+import Peer from 'peerjs';
+
+ var conn = null;
 
 class Player extends Component {
+
+
+    static defaultProps = {
+      peer: new Peer(null, {
+          debug: 2
+      })
+    }
+
+    state = {
+      peer: new Peer(null, {
+          debug: 2
+      }),
+      conn: null,
+      players: [],
+      hostId: ''
+    }
+
+    handleInput(event) {
+      this.setState({hostId: event.target.value});
+
+    }
+
+    handleSubmit(event) {
+      console.log("A host ID was submitted");
+      event.preventDefault();
+      this.join();
+
+    }
+
+    initialize = () => {
+      // Create own peer object with connection to shared PeerJS server
+    //  this.setState({
+
+    //  })
+
+
+      this.state.peer.on('open', (id) => {
+        // Workaround for peer.reconnect deleting previous id
+        // modified for react, vanilla JS credit to: https://github.com/jmcker/Peer-to-Peer-Cue-System
+        /*if (this.state.peer.id === null) {
+            console.log('Received null id from peer open');
+            this.setState({peer.id: lastPeerId});
+        } else {
+            lastPeerId = this.props.peer.id;
+        }*/
+
+        //handle connection message
+        console.log("ID: " + this.props.peer.id);
+
+
+      });
+
+      this.props.peer.on('disconnected', () => {
+        //handle connection message
+        console.log("Connection lost. Please reconnect");
+
+
+      // Workaround for peer.reconnect deleting previous id
+      // modified for react, vanilla JS credit to: https://github.com/jmcker/Peer-to-Peer-Cue-System
+      /*this.setState({
+        peer.id: lastPeerId;
+      })
+      peer._lastServerId = lastPeerId;*/
+        this.state.peer.reconnect();
+      });
+
+      this.state.peer.on('close', () => {
+        conn = null;
+        //handle connection message
+        console.log('Connection destroyed');
+      });
+
+      this.state.peer.on('error', (err) => {
+        console.log(err);
+        alert(''+ err);
+      })
+    }
+
+    componentDidMount() {
+
+      this.initialize();
+    }
+
+    join = () => {
+      //close old connection
+      if(conn) {
+        conn.close();
+      }
+      // Create connection to destination peer specified in the input field
+      conn = this.state.peer.connect(this.state.hostId, {
+          reliable: true
+      });
+
+      conn.on('open', () => {
+        //handle connection messages
+        console.log("Connected to: " + conn.peer);
+      // Check URL params for comamnds that should be sent immediately
+      var command = this.getUrlParam("command");
+      if (command) {
+        conn.send(command);
+      }
+    });
+    // Handle incoming data
+    conn.on('data', function (data) {
+        console.log("data received", data);
+        switch (data) {
+
+            default:
+                console.log(data);
+                //addMessage("<span class=\"peerMsg\">Peer: </span>" + data);
+                break;
+        };
+        //addMessage("<span class=\"peerMsg\">Peer:</span> " + data);
+        console.log("message sent");
+        });
+        conn.on('close', function () {
+          //handle connection message
+          console.log("connection closed");
+        });
+
+      }
+
+      getUrlParam = (name) => {
+          name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+          var regexS = "[\\?&]" + name + "=([^&#]*)";
+          var regex = new RegExp(regexS);
+          var results = regex.exec(window.location.href);
+          if (results == null)
+              return null;
+          else
+              return results[1];
+      }
+
+
+
+
   render() {
     return (
       <div className="Player">
@@ -12,9 +151,18 @@ class Player extends Component {
         </header>
         <main className="main">
           <section className="connection-info">
-            <label className="connection-label" htmlFor="host-id">Input the ID provided by your host here and press connect.</label>
-            <input className="connection-input" type="text" name="host-id" id="host-id" />
-            <button className="connection-btn">Connect</button>
+            <form className="connect-form" onSubmit={(evt) => this.handleSubmit(evt)}>
+              <label className="connection-label" htmlFor="host-id">Input the ID provided by your host here and press connect.</label>
+              <input
+                className="connection-input"
+                type="text"
+                name="host-id"
+                id="host-id"
+                value={this.state.hostId}
+                onChange={(evt) => this.handleInput(evt)}
+                />
+              <input className="connection-btn" type="submit" value="Connect" />
+            </form>
             <div className="connection-status">Connection Status: </div>
             <div className="player-status"></div>
           </section>
